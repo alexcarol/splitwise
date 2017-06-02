8176ec6b1463aaab458b0a57af885bb4ff7d7776
Alex Carol <alex.carol.c@gmail.com>
2017-06-02 23:07:45 +0200

    wip

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/gob"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
@@ -9,6 +10,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/mrjones/oauth"
 	"github.com/pkg/browser"
@@ -30,7 +32,8 @@ func main() {
 	if len(os.Args) < 2 {
 		fmt.Print("A subcommand must be invoked, subcommands available:\n\n")
 		fmt.Println("test", "tests the app")
-		fmt.Println("groups", "lists the groups you belong to")
+		fmt.Println("add", "adds a new expense")
+		fmt.Println("get", "gets all the expenses for a group")
 
 		fmt.Println()
 		os.Exit(2)
@@ -51,33 +54,169 @@ func main() {
 	case "test":
 		err := test(consumer)
 		if err != nil {
-			fmt.Printf("Error authenticating: %v", err)
+			printToErr(fmt.Sprintf("Error authenticating: %v", err))
 			os.Exit(1)
 		}
-	case "groups":
-		accessToken, err := getAccessToken(consumer)
+	case "add":
+		err := addExpense(consumer)
 		if err != nil {
-			fmt.Printf("failed to authenticated, gotten: %v", err)
+			printToErr(err)
 			os.Exit(1)
-		}
-		response, err := consumer.Get(urlPrefix+"get_groups", nil, accessToken)
-		if err != nil {
-			fmt.Printf("failed to request groups, gotten: %v", err)
-			os.Exit(1)
-		}
-		content, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			panic(err)
-		}
 
-		// TODO decode groups properly and present content nicely
-		fmt.Println(string(content))
+		}
+	case "get":
+		err := getExpenses(consumer)
+		if err != nil {
+			printToErr(err)
+			os.Exit(1)
+
+		}
 	default:
-		fmt.Println("Unknown subcommand", os.Args[1])
+		printToErr("Unknown subcommand", os.Args[1])
 		os.Exit(2)
 	}
 }
 
+func printToErr(a ...interface{}) {
+	fmt.Fprintln(os.Stderr, a)
+}
+
+type group struct {
+	Name    string   `json:"name"`
+	Members []member `json:"members"`
+	ID      int      `json:"id"`
+}
+
+type member struct {
+	ID        memberID  `json:"id"`
+	LastName  string    `json:"last_name"`
+	FirstName string    `json:"first_name"`
+	Balance   []balance `json:"balance"`
+}
+
+type balance struct {
+	Amount       string `json:"amount"`
+	CurrencyCode string `json:"currency_code"`
+}
+
+type memberID int
+
+func getExpenses(consumer *oauth.Consumer) error {
+	accessToken, err := getAccessToken(consumer)
+	if err != nil {
+		return fmt.Errorf("failed to authenticated, gotten: %v", err)
+	}
+
+	response, err := consumer.Get(urlPrefix+"get_groups", nil, accessToken)
+	if err != nil {
+		return fmt.Errorf("failed to request groups, gotten: %v", err)
+	}
+
+	var groupJSON struct {
+		Groups []group
+	}
+	err = json.NewDecoder(response.Body).Decode(&groupJSON)
+	response.Body.Close()
+	if err != nil {
+		return err
+	}
+	for i, g := range groupJSON.Groups {
+		fmt.Fprintf(os.Stderr, "%d.%s\n", i, g.Name)
+	}
+	printToErr("Choose a group:")
+
+	var groupNum int
+	fmt.Scan(&groupNum)
+
+	params := map[string]string{
+		"payment":     "1",
+		"cost":        "123",
+		"description": "test description",
+		"group_id":    strconv.Itoa(groupJSON.Groups[groupNum].ID),
+	}
+
+	response, err = consumer.Post(urlPrefix+"create_expense", params, accessToken)
+	if err != nil {
+		return err
+	}
+	content, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+	// TODO decode groups properly and present content nicely
+	fmt.Println(string(content))
+
+	return err
+}
+
+func addExpense(consumer *oauth.Consumer) error {
+	accessToken, err := getAccessToken(consumer)
+	if err != nil {
+		return fmt.Errorf("failed to authenticated, gotten: %v", err)
+	}
+
+	response, err := consumer.Get(urlPrefix+"get_groups", nil, accessToken)
+	if err != nil {
+		return fmt.Errorf("failed to request groups, gotten: %v", err)
+	}
+
+	var groupJSON struct {
+		Groups []group
+	}
+	err = json.NewDecoder(response.Body).Decode(&groupJSON)
+	response.Body.Close()
+	if err != nil {
+		return err
+	}
+	for i, g := range groupJSON.Groups {
+		fmt.Fprintf(os.Stderr, "%d.%s\n", i, g.Name)
+	}
+	printToErr("Choose a group:")
+
+	var groupNum int
+	fmt.Scan(&groupNum)
+
+	params := map[string]string{
+		"payment":     "1",
+		"cost":        "123",
+		"description": "test description",
+		"group_id":    strconv.Itoa(groupJSON.Groups[groupNum].ID),
+	}
+
+	response, err = consumer.Post(urlPrefix+"create_expense", params, accessToken)
+	if err != nil {
+		return err
+	}
+	content, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+	// TODO decode groups properly and present content nicely
+	fmt.Println(string(content))
+
+	return err
+}
+
+func groups(consumer *oauth.Consumer) error {
+	accessToken, err := getAccessToken(consumer)
+	if err != nil {
+		return fmt.Errorf("failed to authenticated, gotten: %v", err)
+	}
+	response, err := consumer.Get(urlPrefix+"get_groups", nil, accessToken)
+	if err != nil {
+		return fmt.Errorf("failed to request groups, gotten: %v", err)
+	}
+	defer response.Body.Close()
+
+	content, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+	// TODO decode groups properly and present content nicely
+	fmt.Println(string(content))
+
+	return nil
+}
 func getAccessToken(consumer *oauth.Consumer) (*oauth.AccessToken, error) {
 	// TODO check storage
 	file, err := os.Open("/tmp/splitwise")
